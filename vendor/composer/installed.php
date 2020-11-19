83a1983b93d28b1c23e333205fac9d2c56f0438b
Enriko Todorov <impossiblelimner@gmail.com>
2020-11-19 21:33:27 +0000

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'No version set (parsed as 1.0.0)',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'No version set (parsed as 1.0.0)',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'austinb/gameq' => 
+    array (
+      'pretty_version' => 'v3.0.15',
+      'version' => '3.0.15.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c62a7b46e5fe0149087d7f69cce146a31d457d3e',
+    ),
+  ),
+);

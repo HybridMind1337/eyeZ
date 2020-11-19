e94fbd89fa200f22706fb2c147f59a89bdf5aed9
Enriko Todorov <impossiblelimner@gmail.com>
2020-11-19 22:17:15 +0000

    Update sql.sql

--- install/sql.sql
+++ install/sql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `eyez_servers` (
+CREATE TABLE IF NOT EXISTS `eyez_servers` (
  `id` int(12) NOT NULL AUTO_INCREMENT,
   `ip` text NOT NULL,
   `port` text NOT NULL,
@@ -8,5 +8,6 @@ CREATE TABLE `eyez_servers` (
   `map` text NOT NULL,
   `hostname` text NOT NULL,
   `status` text NOT NULL,
-  `last_update` int(11) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `last_update` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8  AUTO_INCREMENT=11 ;

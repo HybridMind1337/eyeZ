70832398a7e698c38a2b214e8f7c69b433ce0609
Enriko Todorov <impossiblelimner@gmail.com>
2021-02-20 21:33:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
+/*
+ *
+ *  Project Name: eyeZ - Game Server Monitoring
+ *  Author: HybridMind <www.webocean.info>
+ *  Version: 2.0.1
+ *  License:  GPL-3.0
+ *  Discord: HybridMind#6095
+ *
+ */
+
 if (file_exists('install')) {
     header('location: install/');
     exit();
-}
+} else {
     header("Location: admin/index.php");
     exit();
-
-
+}
